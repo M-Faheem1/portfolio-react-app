c64c59c43e081462d17ed1c962c0737becbe90b2
Faheem <faheemiqbal0101@gmail.com>
2023-06-12 12:08:19 +0500

    changing images path

--- src/pages/homePage/components/WorkCardListing.jsx
+++ src/pages/homePage/components/WorkCardListing.jsx
@@ -6,18 +6,18 @@ function WorkCardListing() {
 		<div className='my-work__cards__list'>
 			<CustomCard
 				cardDescription={'Personal Portfolio Website Hero Design Concept'}
-				imageSource={'/src/assets/images/card1.png'}
+				imageSource={'/public/images/card1.png'}
 				alt={'image-1'}
 			/>
 			<CustomCard
 				cardDescription={'Personal Portfolio Website Hero Design Concept'}
-				imageSource={'/src/assets/images/card2.png'}
+				imageSource={'/public/images/card2.png'}
 				alt={'image-2'}
 			/>
 
 			<CustomCard
 				cardDescription={'Personal Portfolio Website Hero Design Concept'}
-				imageSource={'/src/assets/images/card3.png'}
+				imageSource={'/public/images/card3.png'}
 				alt={'image-3'}
 			/>
 		</div>
